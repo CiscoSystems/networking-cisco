5c1bccf089fa38f4a8b39416ad9254bdbfc19512
Kent Wu <wu@noironetworks.com>
2016-06-21 11:32:47 -0700

    Added sub-interface configuration
    This patch adds the ability to configure sub-
    interfaces using configuration information
    provided by the users.

--- networking_cisco/tests/unit/cisco/cfg_agent/test_aciasr1k_routing_driver.py
+++ networking_cisco/tests/unit/cisco/cfg_agent/test_aciasr1k_routing_driver.py
@@ -66,6 +66,20 @@ class ASR1kRoutingDriverAci(asr1ktest.ASR1kRoutingDriver):
         self.transit_gw_vip = '1.103.2.2'
         self.transit_cidr = '1.103.2.1/24'
         self.transit_vlan = '1035'
+        self.int_port_w_config = (
+            {'id': PORT_ID,
+             'ip_cidr': self.gw_ip_cidr,
+             'fixed_ips': [{'ip_address': self.gw_ip}],
+             'subnets': [{'cidr': self.gw_ip_cidr,
+                         'gateway_ip': self.gw_ip}],
+             'hosting_info': {
+                 'physical_interface': self.phy_infc,
+                 'segmentation_id': self.transit_vlan,
+                 'gateway_ip': self.transit_gw_ip,
+                 'cidr_exposed': self.transit_cidr,
+                 'interface_config': ['testroute1', 'testroute2']
+             },
+             HA_INFO: self.gw_ha_info})
         self.int_port = {'id': PORT_ID,
                          'ip_cidr': self.gw_ip_cidr,
                          'fixed_ips': [{'ip_address': self.gw_ip}],
@@ -126,6 +140,31 @@ class ASR1kRoutingDriverAci(asr1ktest.ASR1kRoutingDriver):
             snippets.SET_TENANT_ROUTE_WITH_INTF, cfg_args_route)
 
         sub_interface = self.phy_infc + '.' + str(self.transit_vlan)
+        mask = netaddr.IPNetwork(self.transit_cidr).netmask
+        cfg_args_sub = (sub_interface, self.transit_vlan, self.vrf,
+                        self.transit_cidr.split("/")[0], mask)
+        self.assert_edit_run_cfg(
+            asr_snippets.CREATE_SUBINTERFACE_WITH_ID, cfg_args_sub)
+
+        cfg_args_hsrp = self._generate_hsrp_cfg_args(
+            sub_interface, self.gw_ha_group,
+            self.ha_priority, self.transit_gw_vip,
+            self.transit_vlan)
+        self.assert_edit_run_cfg(
+            asr_snippets.SET_INTC_ASR_HSRP_EXTERNAL, cfg_args_hsrp)
+
+    def test_internal_network_added_with_interface_config(self):
+        cfg.CONF.set_override('enable_multi_region', False, 'multi_region')
+        self.port = self.int_port_w_config
+        self.driver.internal_network_added(self.ri, self.port)
+        sub_interface = self.phy_infc + '.' + str(self.transit_vlan)
+        cfg_args_route = (sub_interface, 'testroute1')
+        self.assert_edit_run_cfg(
+            snippets.SET_INTERFACE_CONFIG, cfg_args_route)
+        cfg_args_route = (sub_interface, 'testroute2')
+        self.assert_edit_run_cfg(
+            snippets.SET_INTERFACE_CONFIG, cfg_args_route)
+
         mask = netaddr.IPNetwork(self.transit_cidr).netmask
         cfg_args_sub = (sub_interface, self.transit_vlan, self.vrf,
                         self.transit_cidr.split("/")[0], mask)
@@ -214,6 +253,20 @@ class ASR1kRoutingDriverAci(asr1ktest.ASR1kRoutingDriver):
         self.driver._do_remove_sub_interface = mock.MagicMock()
         self.driver.internal_network_removed(self.ri, self.port)
         self.assertFalse(self.driver._do_remove_sub_interface.called)
+
+    def test_internal_network_removed_with_interface_config(self):
+        self.port = self.int_port_w_config
+        self.driver._do_remove_sub_interface = mock.MagicMock()
+        self.driver.internal_network_removed(self.ri, self.port)
+        self.assertFalse(self.driver._do_remove_sub_interface.called)
+
+        sub_interface = self.phy_infc + '.' + str(self.transit_vlan)
+        cfg_args_route = (sub_interface, 'testroute1')
+        self.assert_edit_run_cfg(
+            snippets.REMOVE_INTERFACE_CONFIG, cfg_args_route)
+        cfg_args_route = (sub_interface, 'testroute2')
+        self.assert_edit_run_cfg(
+            snippets.REMOVE_INTERFACE_CONFIG, cfg_args_route)
 
     def _next_ip(self, curr_ip):
         ip = netaddr.IPAddress(curr_ip)
